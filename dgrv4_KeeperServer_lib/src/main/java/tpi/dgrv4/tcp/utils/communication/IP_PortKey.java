dffe64dc7812d3f827277aa9aacb3f979ef981ea
mini.lee <mini.lee@tpisoftware.com>
2025-07-24 11:24:38 +0800

    fix vulnerabilities and upgrade backend dependency packages

--- dgrv4_KeeperServer_lib/src/main/java/tpi/dgrv4/tcp/utils/communication/IP_PortKey.java
+++ dgrv4_KeeperServer_lib/src/main/java/tpi/dgrv4/tcp/utils/communication/IP_PortKey.java
@@ -1,7 +1,7 @@
 package tpi.dgrv4.tcp.utils.communication;
 
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 public class IP_PortKey {
 	public String ip;
